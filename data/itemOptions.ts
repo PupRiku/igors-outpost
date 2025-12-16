1c45e6fa06742630a39f597778904226f8fcc72c
Chris Diorio <11452673+PupRiku@users.noreply.github.com>
2025-12-16 17:14:15 +0000

    feat: add ITEM_VARIANTS data structure for item options

---
+++ data/itemOptions.ts
@@ -0,0 +1,29 @@
+export const ITEM_VARIANTS: Record<string, string[]> = {
+  "Ammunition, +1 (each)": [
+    "Arrow",
+    "Bolt",
+    "Sling Bullet",
+    "Blowgun Needle"
+  ],
+  "Ammunition, +2 (each)": [
+    "Arrow",
+    "Bolt",
+    "Sling Bullet",
+    "Blowgun Needle"
+  ],
+  "Elemental gem": [
+    "Blue Sapphire (Air)",
+    "Yellow Diamond (Earth)",
+    "Red Corundum (Fire)",
+    "Emerald (Water)"
+  ],
+  "Potion of resistance": [
+    "Acid", "Cold", "Fire", "Force", "Lightning", 
+    "Necrotic", "Poison", "Psychic", "Radiant", "Thunder"
+  ],
+  "Scroll of protection": [
+    "Aberrations", "Beasts", "Celestials", "Constructs", 
+    "Dragons", "Elementals", "Fey", "Fiends", "Giants", 
+    "Monstrosities", "Oozes", "Plants", "Undead"
+  ]
+};
